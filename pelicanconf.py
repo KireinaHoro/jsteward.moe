bd6c37709bc6c24aa36f822dde0f62dbe2ce80de
Johnson Steward <xubin990510@gmail.com>
2017-01-13 18:24:08 +0900

    switch to built-texts theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -12,7 +12,7 @@ TIMEZONE = 'Asia/Shanghai'
 
 DEFAULT_LANG = 'zh'
 
-THEME = 'themes/pelican-blue'
+THEME = 'themes/built-texts'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
