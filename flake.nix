cd1e4f793460473a65e88f4cad2e4f2abcd60026
Pengcheng Xu <pengxu@ethz.ch>
2024-10-16 09:34:40 +0200

    remove unnecessary patch
    Former-commit-id: 8d8d38bf99a81790a8fd20b8a681ffa8c7e0b4d9
    
    Former-commit-id: 9740c82fba09d323bf8e0874701cffa130fd57f6

--- flake.nix
+++ flake.nix
@@ -13,39 +13,6 @@
     checks = { site = pkgs.jstewardMoe; };
   }) // {
     overlay = final: prev: {
-      /*
-      python = prev.python.override {
-        packageOverrides = self: super: {
-          pelican = super.pelican.overrideAttrs (oldAttrs: rec {
-            patches = prev.writeText "jinja2-markupsafe.patch" ''
-              --- source/pelican/utils.py     2021-08-01 18:33:58.588682264 +0800
-              +++ source-new/pelican/utils.py 2021-08-01 18:33:43.398933115 +0800
-              @@ -18,7 +18,7 @@
-
-               import dateutil.parser
-
-              -from jinja2 import Markup
-              +from markupsafe import Markup
-
-               import pytz
-
-              diff -Naur source/pelican/writers.py source-new/pelican/writers.py
-              --- source/pelican/writers.py   2021-08-01 18:34:10.770481673 +0800
-              +++ source-new/pelican/writers.py       2021-08-01 18:33:43.398933115 +0800
-              @@ -5,7 +5,7 @@
-
-               from feedgenerator import Atom1Feed, Rss201rev2Feed, get_tag_uri
-
-              -from jinja2 import Markup
-              +from markupsafe import Markup
-
-               from pelican.paginator import Paginator
-               from pelican.plugins import signals
-
-            '';
-          });
-        };
-      }; */
       jstewardMoe = with final; stdenv.mkDerivation {
         name = "jsteward.moe";
         src = lib.cleanSource ./.;
