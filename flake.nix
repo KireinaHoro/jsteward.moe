041843f3e5e2ea40627586a9a01f53b582ee70c7
Pengcheng Xu <pengxu@ethz.ch>
2026-04-21 15:50:58 +0200

    build cv in flake

--- flake.nix
+++ flake.nix
@@ -4,20 +4,39 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    resume = {
+      url = "github:KireinaHoro/resume";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = inputs@{ self, nixpkgs, flake-utils }:
+  outputs = inputs@{ self, nixpkgs, flake-utils, resume }:
   with flake-utils.lib; eachSystem (defaultSystems ++ ["aarch64-darwin"]) (system: let
-    pkgs = import nixpkgs { inherit system; overlays = [ self.overlay ]; };
-  in rec {
-    defaultPackage = pkgs.jstewardMoe;
-    checks = { site = pkgs.jstewardMoe; };
+    pkgs = import nixpkgs { inherit system; };
+    cv = "${resume.packages.${system}.default}/cv.pdf";
+    website = pkgs.stdenvNoCC.mkDerivation {
+      name = "jsteward.moe";
+      src = pkgs.lib.cleanSource ./.;
+      buildInputs = with pkgs.python3Packages; [
+        pelican
+        markdown
+        pillow
+      ];
+      buildPhase = ''
+        mkdir -p $out
+        pelican
+        cp ${cv} $out/images/
+      '';
+      COMMIT = if self ? rev then self.rev else "dirty";
+    };
+  in {
+    checks = { inherit website; };
     apps.default = let
       serve = pkgs.writeShellApplication {
         # serve the built local tree on port 8080
         name = "serve";
-        runtimeInputs = [pkgs.caddy];
+        runtimeInputs = [ pkgs.caddy ];
         text = ''
-          caddy file-server --listen localhost:8880 --root ${pkgs.jstewardMoe} --access-log
+          caddy file-server --listen localhost:8880 --root ${website} --access-log
         '';
       };
     in {
@@ -27,22 +46,6 @@
     devShells.default = pkgs.mkShell {
       buildInputs = with pkgs; [ getopt ];
     };
-  }) // {
-    overlay = final: prev: {
-      jstewardMoe = with final; stdenvNoCC.mkDerivation {
-        name = "jsteward.moe";
-        src = lib.cleanSource ./.;
-        buildInputs = with python3Packages; [
-          pelican
-          markdown
-          pillow
-        ];
-        buildPhase = ''
-          mkdir -p $out
-          pelican
-        '';
-        COMMIT = if self ? rev then self.rev else "dirty";
-      };
-    };
-  };
+    packages.default = website;
+  });
 }
